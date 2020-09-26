55a20fc212dc5f61f4107eb62548b6232905e5f9
logankim0212 <logan.kim0212@gmail.com>
2020-09-26 01:08:44 -0400

    login return type bug fixed

--- src/main/java/com/covidtrail/covidtrailbackend/controller/AuthenticationController.java
+++ src/main/java/com/covidtrail/covidtrailbackend/controller/AuthenticationController.java
@@ -1,5 +1,6 @@
 package com.covidtrail.covidtrailbackend.controller;
 
+import com.covidtrail.covidtrailbackend.dto.AccountDetailsDto;
 import com.covidtrail.covidtrailbackend.dto.LoginDto;
 import com.covidtrail.covidtrailbackend.repository.AuthenticationService;
 import io.swagger.annotations.Api;
@@ -24,7 +25,7 @@ public class AuthenticationController {
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "Success"),
             @ApiResponse(code = 500, message = "Unexpected error")})
-    public PlacesVisitedLogCreateDto login(@RequestBody LoginDto loginDto) throws Exception {
+    public AccountDetailsDto login(@RequestBody LoginDto loginDto) throws Exception {
         // TODO
         return null;
     }
