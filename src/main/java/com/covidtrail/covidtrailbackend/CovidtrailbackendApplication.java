b038cb37166e742ca5417ef72d682498c36da67b
Alanhslc <alanh.lucena@gmail.com>
2020-09-27 03:29:26 -0400

    added cross origin

--- src/main/java/com/covidtrail/covidtrailbackend/CovidtrailbackendApplication.java
+++ src/main/java/com/covidtrail/covidtrailbackend/CovidtrailbackendApplication.java
@@ -2,10 +2,13 @@ package com.covidtrail.covidtrailbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@CrossOrigin(origins = "https://covidtrail-backend.azurewebsites.net")
 public class CovidtrailbackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CovidtrailbackendApplication.class, args);
